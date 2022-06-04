b9dc49b84cf3a65edd21d838ffc1e0b8ab533693
GabrielVerlyy <gabrielsouzaverly7@gmail.com>
2022-06-04 19:54:05 -0300

    lab_dom

--- lab_dom/js/app.js
+++ lab_dom/js/app.js
@@ -14,9 +14,102 @@ const soma = (number1, number2) => {
     return number1 + number2;
 }
 
-document.getElementById("button").addEventListener("click", () => {
+document.getElementById("bt1").addEventListener("click", () => {
     const att11 = document.getElementById("onenumber");
     const att12 = document.getElementById("twonumber");
     const res1 = document.getElementById("resposta1");
     res1.innerHTML = soma(parseFloat(att11.value), parseFloat(att12.value))
+})
+
+//Atividade 2:
+
+const maiormenor = (num21, num22) => {
+    if (num21 > num22) {
+        return 'O número um é o maior.';
+    } else {
+        return 'O número dois é o maior';
+    }
+}
+
+document.getElementById("bt2").addEventListener("click", () => {
+
+    const att21 = document.getElementById("tnumber")
+    const att22 = document.getElementById("t2number")
+    const res2 = document.getElementById("res2")
+    res2.innerHTML = maiormenor(parseFloat(att21.value), parseFloat(att22.value))
+
+})
+
+//Atividade 3:
+
+const primo = (num31) => {
+    for (let i = 2; num31 >= i; i++) {
+        if (num31 % 2 === 0 && num31 % 3 === 0 && num31 % 5 === 0) {
+            return 'O número não é primo'
+        }
+        if (num31 % i === 0) {
+            return 'O número não é primo'
+        } else {
+            return 'O número é primo'
+        }
+    }
+}
+
+document.getElementById("bt3").addEventListener("click", () => {
+    const att31 = document.getElementById("3number")
+    const res3 = document.getElementById("res3")
+    res3.innerHTML = primo(parseFloat(att31.value))
+})
+
+//Atividade 4:
+
+const hipotenusa = (num41, num42) => {
+    return num41 * num41 + num42 * num42
+}
+
+document.getElementById("bt4").addEventListener("click", () => {
+    const att41 = document.getElementById("41number")
+    const att42 = document.getElementById("42number")
+    const res4 = document.getElementById("res4")
+    res4.innerHTML = hipotenusa(parseFloat(att41.value), parseFloat(att42.value))
+})
+
+//Atividade 5:
+
+const salario = (num5) => {
+    return num5 * 0.11
+}
+
+document.getElementById("bt5").addEventListener("click", () => {
+    const att51 = document.getElementById("5number")
+    const res5 = document.getElementById("res5")
+    res5.innerHTML = salario(parseFloat(att51.value))
+})
+
+//Atividade 6:
+
+const grau = (num6) => {
+    return num6 * 1.8 + 32
+}
+
+document.getElementById("bt6").addEventListener("click", () => {
+
+    const att6 = document.getElementById("6number")
+    const res6 = document.getElementById("res6")
+    res6.innerHTML = grau(parseFloat(att6.value))
+})
+
+//Atividade 7
+
+const nota = (num71, num72, num73) => {
+    return num71 * 2 + num72 * 3 + num73 * 5 / 10
+}
+
+document.getElementById("bt7").addEventListener("click", () => {
+
+    const att71 = document.getElementById("71number")
+    const att72 = document.getElementById("72number")
+    const att73 = document.getElementById("73number")
+    const res7 = document.getElementById("res7")
+    res7.innerHTML = grau(parseFloat(att71.value), parseFloat(att72.value), parseFloat(att73.value))
 })
