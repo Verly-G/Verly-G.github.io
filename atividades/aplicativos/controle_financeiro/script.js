b4ea79e514806ac9746bca715df7eb8efc97da1a
GabrielSouzaVerly <gabrielverly03@gmail.com>
2022-12-16 18:45:20 -0300

    Aplicativo Financeiro

--- atividades/aplicativos/controle_financeiro/script.js
+++ atividades/aplicativos/controle_financeiro/script.js
@@ -1,12 +1,108 @@
-const dummyTransactions = [
-    {id: 1, name: 'Bolo de brigadeiro', amount: -20},
-    {id: 2, name: 'Salário', amount: 300},
-    {id: 3, name: 'Torta de Frango', amount: -10},
-    {id: 4, name: 'Violão', amount: 150}
-]
+const transactionUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount = document.querySelector('#amount')
 
-const addTransactionIntoDOM = transaction => {
-    <li class="minus">
-        Salário <span>-$400</span><button class="delete-btn">x</button>
-    </li> 
+const localStoregeTransactions = JSON.parse(localStorage
+    .getItem('transactions'))
+let transactions = localStorage
+    .getItem('transactions') !== null ? localStoregeTransactions : []
+
+const removeTransaction = ID =>{
+    transactions = transactions.filter(transaction => 
+        transaction.id !== ID)
+    updateLocalStorage()
+    init()
 }
+
+const addTransactionIntoDOM = ({ amount, name, id }) => {
+    const operator = amount < 0 ? '-' : '+'
+    const CSSClass = amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(amount)
+    const li = document.createElement('li')
+    
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+        ${name} 
+        <span>${operator} R$ ${amountWithoutOperator}</span>
+        <button class="delete-btn" onClick="removeTransaction(${id})">x</button>
+    `
+    transactionUl.prepend(li) 
+}
+
+const getExpenses = transactionAmounts => Math.abs(transactionAmounts
+    .filter(value => value < 0)
+    .reduce((accumulator, value) => accumulator + value, 0))
+    .toFixed(2)
+
+const getIncome = transactionAmounts => transactionAmounts
+    .filter(value => value > 0)
+    .reduce((accumulator, value) => accumulator + value, 0)
+    .toFixed(2)
+
+const getTotal = transactionAmounts => transactionAmounts
+    .reduce((accumulator, transaction) => accumulator + transaction, 0)
+    .toFixed(2)
+
+const updateBalanceValues = () => {
+    const transactionAmounts = transactions.map(({ amount }) => amount)
+
+    const total = getTotal(transactionAmounts)
+    const income = getIncome(transactionAmounts)
+    const expense = getExpenses(transactionAmounts)
+
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
+}
+
+const init = () => {
+    transactionUl.innerHTML = ''
+    transactions.forEach(addTransactionIntoDOM)
+    updateBalanceValues()
+}
+
+init()
+
+const updateLocalStorage = () =>{
+    localStorage.setItem('transactions', JSON.stringify(transactions))
+}
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+const addToTransactionArray = (transactionName, transactionAmount) => {
+    transactions.push({
+        id: generateID(), 
+        name: transactionName, 
+        amount: Number(transactionAmount)
+    })
+
+}
+
+const cleanInputs = () =>{
+    inputTransactionName.value = ''
+    inputTransactionAmount.value = ''
+}
+
+const handleFromSubmit = event => {
+    event.preventDefault()
+
+    const transactionName = inputTransactionName.value.trim()
+    const transactionAmount = inputTransactionAmount.value.trim()
+    const isSomeInputEmpty = transactionName === '' || transactionAmount === ''
+    
+    if(isSomeInputEmpty){
+        alert('Por favor, preencha o nome e o valor da transação')
+        return
+    }
+
+    addToTransactionArray(transactionName, transactionAmount)
+    init()
+    updateLocalStorage()
+    cleanInputs()
+}
+
+form.addEventListener('submit', handleFromSubmit)
