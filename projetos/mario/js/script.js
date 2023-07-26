858f7defd70f62eae3210d2fa30fb5f4460fecff
Verly-G <gabrielverly03@gmail.com>
2023-07-26 08:06:51 -0300

    Mario Jump finalizado

--- projetos/mario/js/script.js
+++ projetos/mario/js/script.js
@@ -2,8 +2,12 @@ const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
 const clouds = document.querySelector('.clouds')
 const button = document.querySelector('.btnReiniciar')
+let segundos = document.querySelector('#segundos')
+let minutos = document.querySelector('#wminutos')
 let perdeuJogo = false
-
+let milisegundos = 0
+let segundo = 0
+let minuto = 0
 const jump = () =>{
     mario.classList.add('jump')
 
@@ -22,6 +26,25 @@ const loop = setInterval(() =>{
     const pipePosition = pipe.offsetLeft
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
     const cloudsPosition = clouds.offsetLeft
+
+
+    milisegundos +=10
+    if(milisegundos == 1000){
+        milisegundos = 0
+        segundo +=1
+        segundos.innerHTML = segundo
+        if(segundo < 10){
+            segundos.innerHTML = `0${segundo}` 
+        }
+    }
+    if(segundos == 60){
+        segundo = 0
+        minuto += 1
+        minutos.innerHTML = minuto
+        if(minuto < 10){
+            minutos.innerHTML = `0${minuto}` 
+        }
+    }
 
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
         pipe.style.animation = 'none'
